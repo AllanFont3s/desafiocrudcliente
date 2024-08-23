067a2bc98acc0b10f6e16eb273d565c8809d795a
Allan Fontes <allanfontes@gmail.com>
2024-08-23 11:00:00 -0300

    Start

---
+++ src/main/java/com/devesuperior/desafiocrudcliente/DesafiocrudclienteApplication.java
@@ -0,0 +1,13 @@
+package com.devesuperior.desafiocrudcliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiocrudclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiocrudclienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devesuperior/desafiocrudcliente/DesafiocrudclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devesuperior.desafiocrudcliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiocrudclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
