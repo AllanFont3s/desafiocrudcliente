1665f5183058f913a92c5f554005046a0994c1f1
Allan Fontes <allanfontes@gmail.com>
2024-08-23 14:35:20 -0300

    crud basic

--- src/main/java/com/devesuperior/desafiocrudcliente/controller/ClientController.java
+++ src/main/java/com/devesuperior/desafiocrudcliente/controller/ClientController.java
@@ -1,14 +1,16 @@
 package com.devesuperior.desafiocrudcliente.controller;
 
 import com.devesuperior.desafiocrudcliente.dto.ClientDTO;
+import com.devesuperior.desafiocrudcliente.entities.Client;
 import com.devesuperior.desafiocrudcliente.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/client")
@@ -18,14 +20,32 @@ public class ClientController {
     private ClientService clientService;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<ClientDTO> findById(Long id) {
-        ClientDTO dto =clientService.findById(id);
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        ClientDTO dto = clientService.findById(id);
         return ResponseEntity.ok(dto);
     }
 
     @GetMapping
-    public ResponseEntity<Page<ClientDTO>> findById(Pageable pageable) {
-        Page<ClientDTO dto> =clientService
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> dto = clientService.findAll(pageable);
         return ResponseEntity.ok(dto);
+    }
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+        dto = clientService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    public ResponseEntity<ClientDTO>update(@RequestBody ClientDTO dto, @PathVariable Long id){
+        dto = clientService.update(dto,id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable  Long id){
+        clientService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/devesuperior/desafiocrudcliente/services/ClientService.java
+++ src/main/java/com/devesuperior/desafiocrudcliente/services/ClientService.java
@@ -7,6 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class ClientService {
@@ -14,15 +19,48 @@ public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
 
-    public ClientDTO findById(Long id){
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
         Client client = clientRepository.findById(id).get();
         return new ClientDTO(client);
     }
 
-    public Page<ClientDTO> findByAll(Pageable peageable){
-        Page<Client> client = clientRepository.findAll(peageable);
-        return client.map(x-> new ClientDTO(x));
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> client = clientRepository.findAll(pageable);
+        return client.map(x -> new ClientDTO(x));
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto) {
+        Client entity = new Client();
+
+        entityToDTO(entity, dto);
+
+        entity = clientRepository.save(entity);
+        return new ClientDTO(entity);
     }
 
 
+    @PutMapping(value = "/{id}")
+    public ClientDTO update(ClientDTO dto, Long id) {
+        Client entity = clientRepository.getReferenceById(id);
+        entityToDTO(entity, dto);
+        entity = clientRepository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    private void entityToDTO(Client entity, ClientDTO dto) {
+        entity.setId(dto.getId());
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void delete(Long id) {
+        clientRepository.deleteById(id);
+    }
 }
